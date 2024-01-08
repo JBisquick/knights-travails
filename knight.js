7546d852be8e4625ae1226478d20275ca7f07fa0
JBisquick <felipebirch@gmail.com>
2024-01-07 16:05:55 -0800

    Add function to validate space

--- knight.js
+++ knight.js
@@ -5,4 +5,27 @@ function space(value, route) {
   }
 }
 
+function knight(start) {
+  const possibleMoves = [
+    [1, 2], [2, 1],
+    [1, -2], [2, -1],
+    [-1, 2], [-2, 1],
+    [-1, -2], [-2, -1]
+  ];
 
+  const validateSpace = (space) => {
+    for (let i of space) {
+      if (i < 0 || i > 7) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  return {
+    validateSpace
+  };
+}
+
+let bob = knight((0, 0));
+console.log(bob.validateSpace([4, 6]));
